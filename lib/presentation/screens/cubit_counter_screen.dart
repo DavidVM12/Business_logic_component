03764d636a997877e8412a39d2e82a7937706763
David Velasco <david.velasco@celuweb.com>
2023-06-10 17:33:41 -0500

    feat: initial esctructure

---
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:forms_app/responsive/responsive.dart';
+
+class CubitCounterScreen extends StatelessWidget {
+  const CubitCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //call method init from ScreenSize class to get screen size
+    ScreenSize.init(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cubit Counter'),
+        actions: [
+          IconButton(
+              onPressed: () => {}, icon: const Icon(Icons.refresh_outlined))
+        ],
+      ),
+      body: const Center(
+        child: Text('Counter value: xxx'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+              heroTag: '1', child: const Text('+3'), onPressed: () => {}),
+          SizedBox(height: ScreenSize.height * 0.02),
+          FloatingActionButton(
+              heroTag: '2', child: const Text('+2'), onPressed: () => {}),
+          SizedBox(height: ScreenSize.height * 0.02),
+          FloatingActionButton(
+              heroTag: '3', child: const Text('+1'), onPressed: () => {}),
+          SizedBox(height: ScreenSize.height * 0.02),
+        ],
+      ),
+    );
+  }
+}
